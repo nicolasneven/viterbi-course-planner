18285caa99fd3e0c8619d509fa1d1494b903f955
Zoe Vogelsang <zoervogelsang@gmail.com>
2019-12-03 01:32:53 -0800

    Create zoetest.java

---
+++ servlets/zoetest.java
@@ -0,0 +1,62 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mongodb.*;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import com.mongodb.client.model.Sorts;
+import java.util.Arrays;
+import org.bson.Document;
+
+/**
+ * Servlet implementation class zoetest
+ */
+@WebServlet("/zoetest")
+public class zoetest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String data = request.getParameter("data");
+		session.setAttribute("data", data);
+		session.setAttribute("test", "TEST");
+         
+         
+	}
+
+}
