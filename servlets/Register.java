b45654d9b5573642544942ec5b9f3318224298e8
niconeven <nneven@usc.edu>
2019-12-03 15:49:43 -0800

    idk

--- servlets/Register.java
+++ servlets/Register.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.mongodb.*;
 import com.mongodb.client.MongoClients;
@@ -55,18 +56,26 @@ public class Register extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		String email = request.getParameter("email");
 		String major = request.getParameter("major");
 		String gradyear = request.getParameter("gradyear");
-				
-		MongoClient mongoClient = MongoClients.create();
+			
+		ConnectionString connString = new ConnectionString(
+				"mongodb+srv://password:username@cluster0-v2kcb.gcp.mongodb.net/test?retryWrites=true&w=majority"
+			);
+		MongoClientSettings settings = MongoClientSettings.builder()
+		    .applyConnectionString(connString)
+		    .retryWrites(true)
+		    .build();
+		MongoClient mongoClient = MongoClients.create(settings);
 		MongoDatabase database = mongoClient.getDatabase("ViterbiSchedule");
 		MongoCollection<Document> majorsCollection = database.getCollection("Majors");
 		MongoCollection<Document> usersCollection = database.getCollection("Users");
 		
-		Document template = majorsCollection.find(eq("_id", major)).first();
+		Document template = majorsCollection.find(eq("_id", "CSCI")).first();
 		
 		Document newUser = new Document("_id", email)
 			.append("name", name)
@@ -74,9 +83,9 @@ public class Register extends HttpServlet {
 			.append("password",password)
 			.append("major", major)
 			.append("gradyear", gradyear)
-			.append("major", template.get("schedule"));
+			.append("schedule", template.get("schedule"));
 		usersCollection.insertOne(newUser);
-		
+		session.setAttribute("user", newUser.toJson());
 		RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/schedule.jsp");
 		dispatch.forward(request,response);
 	}
